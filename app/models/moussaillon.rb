1d05f7d7a55a5bacdb7346e6fc6b5cdf09d3c678
Triqueneaux Calvin <calvin8313@hotmail.fr>
2018-02-13 15:17:07 +0100

    finish without tests

--- app/models/moussaillon.rb
+++ app/models/moussaillon.rb
@@ -1,5 +1,10 @@
 class Moussaillon < ApplicationRecord
   has_many :gossips
+  attr_accessor :sign_up_code
+  validates :sign_up_code,
+  	on: :create,
+  	presence: true,
+  	inclusion: {in: ["GOSSIP_2018"]}
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
